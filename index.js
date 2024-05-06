a180ff3b864fdcdd29f8118f75617bef3048e1c7
GM-11 <mathuyrgopal@gmail.com>
2024-05-06 23:38:42 +0530

    exported app

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import transactionData from "./routes/transactionData.js";
 import ethPrice from "./routes/ethPrice.js";
 import userInfo from "./routes/userInfo.js";
 
-const app = express();
+export const app = express();
 
 app.use(express.json());
 
@@ -29,3 +29,4 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
+
