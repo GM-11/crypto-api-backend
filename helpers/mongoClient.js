513392c0120c91d73ff92cd739b346189862550b
GM-11 <mathuyrgopal@gmail.com>
2024-05-06 22:28:16 +0530

    added mongo client

---
+++ helpers/mongoClient.js
@@ -0,0 +1,20 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+
+if (!uri) {
+  throw new Error("MONGO_URI environment variable is not defined");
+}
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export default client;
